55ce00355d8faa6e8300d7e799fcb9d258c3e0fa
Walter Ching <walter.ching.b@gmail.com>
2019-07-22 17:43:33 +0200

    Update  androidx imports to sample adapters

--- sample/src/main/java/com/rbrooks/indefinitepagerindicatorsample/recyclerViewSample/PhotoItemRecyclerAdapter.kt
+++ sample/src/main/java/com/rbrooks/indefinitepagerindicatorsample/recyclerViewSample/PhotoItemRecyclerAdapter.kt
@@ -2,20 +2,29 @@ package com.rbrooks.indefinitepagerindicatorsample.recyclerViewSample
 
 import android.content.Context
 import android.preference.PreferenceManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.rbrooks.indefinitepagerindicatorsample.R
 import com.rbrooks.indefinitepagerindicatorsample.util.PagerNumberPickerDialogPreference
 import com.rbrooks.indefinitepagerindicatorsample.util.PhotoItem
 
-class PhotoItemRecyclerAdapter(private val context: Context) : RecyclerView.Adapter<PhotoItemViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): PhotoItemViewHolder =
-            PhotoItemViewHolder(LayoutInflater.from(context).inflate(R.layout.viewholder_image_card, parent, false))
+class PhotoItemRecyclerAdapter(private val context: Context) :
+    RecyclerView.Adapter<PhotoItemViewHolder>() {
 
-    override fun onBindViewHolder(holder: PhotoItemViewHolder, position: Int) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoItemViewHolder =
+        PhotoItemViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.viewholder_image_card, parent, false
+            )
+        )
+
+    override fun onBindViewHolder(holder: PhotoItemViewHolder, position: Int) =
         holder.bind(PhotoItem.values()[position])
-    }
 
-    override fun getItemCount(): Int = PreferenceManager.getDefaultSharedPreferences(context).getInt(PagerNumberPickerDialogPreference.KEY_NUM_PAGES, PagerNumberPickerDialogPreference.DEFAULT_PAGES)
+    override fun getItemCount(): Int =
+        PreferenceManager.getDefaultSharedPreferences(context).getInt(
+            PagerNumberPickerDialogPreference.KEY_NUM_PAGES,
+            PagerNumberPickerDialogPreference.DEFAULT_PAGES
+        )
 }
--- sample/src/main/java/com/rbrooks/indefinitepagerindicatorsample/viewPagerSample/ViewPagerAdapter.kt
+++ sample/src/main/java/com/rbrooks/indefinitepagerindicatorsample/viewPagerSample/ViewPagerAdapter.kt
@@ -4,39 +4,44 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.preference.PreferenceManager
-import android.support.v4.view.PagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.viewpager.widget.PagerAdapter
 import com.rbrooks.indefinitepagerindicatorsample.R
 import com.rbrooks.indefinitepagerindicatorsample.util.PagerNumberPickerDialogPreference
 import com.rbrooks.indefinitepagerindicatorsample.util.PhotoItem
 
 class ViewPagerAdapter(private val context: Context) : PagerAdapter() {
 
-    override fun instantiateItem(container: ViewGroup?, position: Int): Any {
-        val view = LayoutInflater.from(context).inflate(R.layout.viewholder_image_card, container, false)
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view =
+            LayoutInflater.from(context).inflate(R.layout.viewholder_image_card, container, false)
         val photoItem = PhotoItem.values()[position]
 
         view.findViewById<ImageView>(R.id.card_imageview).setImageResource(photoItem.photoId)
         view.findViewById<TextView>(R.id.card_title).setText(photoItem.nameId)
         view.findViewById<TextView>(R.id.card_location).setText(photoItem.locationId)
-        view.findViewById<Button>(R.id.card_see_original_button).setOnClickListener({ openLink(photoItem.link) })
+        view.findViewById<Button>(R.id.card_see_original_button)
+            .setOnClickListener { openLink(photoItem.link) }
 
-        container?.addView(view)
+        container.addView(view)
         return view
     }
 
-    override fun destroyItem(container: ViewGroup?, position: Int, `object`: Any?) {
-        container?.removeView(`object` as View)
+    override fun destroyItem(container: ViewGroup, position: Int, someObject: Any) {
+        container.removeView(someObject as View)
     }
 
-    override fun isViewFromObject(view: View?, `object`: Any?): Boolean = view == `object`
+    override fun isViewFromObject(view: View, someObject: Any): Boolean = view == someObject
 
-    override fun getCount(): Int = PreferenceManager.getDefaultSharedPreferences(context).getInt(PagerNumberPickerDialogPreference.KEY_NUM_PAGES, PagerNumberPickerDialogPreference.DEFAULT_PAGES)
+    override fun getCount(): Int = PreferenceManager.getDefaultSharedPreferences(context).getInt(
+        PagerNumberPickerDialogPreference.KEY_NUM_PAGES,
+        PagerNumberPickerDialogPreference.DEFAULT_PAGES
+    )
 
     private fun openLink(link: Uri) {
         context.startActivity(Intent(Intent.ACTION_VIEW, link))
